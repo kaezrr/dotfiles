61c25c6ccaa6b3129783bbb25d36ab4709d1f279
Anjishnu Banerjee <inviktor27@gmail.com>
2025-04-19 23:31:40 +0530

    Update

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,14 @@ return {
 
     -- Useful status updates for LSP.
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-    { 'j-hui/fidget.nvim', opts = {} },
+    {
+      'j-hui/fidget.nvim',
+      opts = {
+        notification = {
+          override_vim_notify = true, -- Automatically override vim.notify() with Fidget
+        },
+      },
+    },
 
     -- Allows extra capabilities provided by nvim-cmp
     'hrsh7th/cmp-nvim-lsp',
