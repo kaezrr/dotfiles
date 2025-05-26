cff3e4b19d6449f3af32676fa95c39fe9991cf82
Anjishnu Banerjee <inviktor27@gmail.com>
2025-05-26 14:14:46 +0530

    update indents

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -24,5 +24,12 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 8
 vim.opt.fillchars = { eob = ' ' }
 vim.opt.confirm = true
+
+-- Indent defaults
 vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+-- Sync clipboard with OS
 vim.opt.clipboard = 'unnamedplus'
