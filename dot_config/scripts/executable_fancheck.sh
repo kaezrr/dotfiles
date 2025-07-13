c4b4f6e81d1ce04b0fe8f491c667206eaa8e866f
Anjishnu Banerjee <inviktor27@gmail.com>
2025-07-13 13:50:27 +0530

    status bar change

--- dot_config/scripts/executable_fancheck.sh
+++ dot_config/scripts/executable_fancheck.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+MAX_RPM=6900
+
 sensors_output=$(sensors)
 
 proc_rpm=$(echo "$sensors_output" | grep "Processor Fan" | sed -E 's/.*Processor Fan:\s*([0-9]+) RPM.*/\1/')
@@ -8,18 +10,30 @@ mobo_rpm=$(echo "$sensors_output" | grep "Motherboard Fan" | sed -E 's/.*Motherb
 proc_rpm=${proc_rpm:-0}
 mobo_rpm=${mobo_rpm:-0}
 
-if [[ $proc_rpm -gt 0 || $mobo_rpm -gt 0 ]]; then
+# Use the higher of the two fans to represent overall cooling activity
+max_rpm=$(( proc_rpm > mobo_rpm ? proc_rpm : mobo_rpm ))
+
+# Clamp max RPM between 0 and MAX_RPM
+clamped_rpm=$(( max_rpm > MAX_RPM ? MAX_RPM : max_rpm ))
+
+# Calculate percentage
+percentage=$(( clamped_rpm * 100 / MAX_RPM ))
+
+# Determine status
+if [[ $max_rpm -gt 0 ]]; then
     status="on"
 else
     status="off"
 fi
 
 jq --unbuffered --compact-output -n \
-  --arg alt "$status" \
-  --arg tooltip "CPU Fan: ${proc_rpm} RPM&#10;GPU Fan: ${mobo_rpm} RPM" \
-  --arg class "$status" \
-  '{
+    --arg alt "$status" \
+    --arg tooltip "CPU Fan: ${proc_rpm} RPM&#10;GPU Fan: ${mobo_rpm} RPM" \
+    --arg class "$status" \
+    --argjson percentage "$percentage" \
+    '{
     alt: $alt,
     tooltip: $tooltip,
-    class: $class
-  }'
+    class: $class,
+    percentage: $percentage
+}'
