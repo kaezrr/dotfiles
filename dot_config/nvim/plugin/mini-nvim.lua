b25165cb8b38ef2e033975a95044bd661f421366
Anjishnu Banerjee <kaezr.dev@gmail.com>
2026-09-04 15:23:50 +0530

    Update .config/nvim/lua/autocmd.lua Update .config/nvim/plugin/mini-nvim.lua

--- dot_config/nvim/plugin/mini-nvim.lua
+++ dot_config/nvim/plugin/mini-nvim.lua
@@ -5,14 +5,10 @@ require('mini.files').setup()
 require('mini.pairs').setup()
 require('mini.statusline').setup()
 require('mini.ai').setup { n_lines = 500 }
-require('mini.starter').setup { footer = '' }
 require('mini.notify').setup { lsp_progress = { duration_last = 3000 } }
 
 vim.keymap.set('n', '<leader>e', function()
-  local current_file = vim.api.nvim_buf_get_name(0)
-  -- If current file is the dashboard open in pwd
-  if vim.bo.filetype == 'ministarter' then current_file = nil end
-  if MiniFiles.close() == nil then MiniFiles.open(current_file) end
+  if MiniFiles.close() == nil then MiniFiles.open(vim.api.nvim_buf_get_name(0)) end
 end)
 
 vim.keymap.set('n', '<leader>n', function() MiniNotify.show_history() end)
